87fedbe6ea9e8d711ea14fd5680143f2e1550773
Marc Valles Casares <13a1911@gmail.com>
2019-08-03 15:00:09 +0200

    hecho hasta la cuarta iteraccion

--- recipes.js
+++ recipes.js
@@ -8,3 +8,38 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+const recipesSchema = new Schema ({
+  title: {type: String, required: true, unique: true},
+  level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
+  ingredients: {type: Array},
+  cuisine: {type: String, required: true},
+  dishType: {type: String, enum: ['Breakfast', 'Dish', 'Snack', 'Drink', 'Dessert', 'Other']},
+  image: {type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration: {type: Number, min: 0},
+  creator: {type: String},
+  created: {type: Date, default: Date.now}
+});
+
+const Recipe = mongoose.model('Recipe', recipesSchema);
+
+Recipe.create({
+  title: 'IronBeer',
+  level: 'Easy Peasy',
+  ingredients: ['Water', 'Barley', 'Hops', 'Yeast'],
+  cuisine: 'Universal',
+  dishType: 'Drink',
+  duration: 20,
+  creator: 'IronMinions'
+}).then(() => {
+  Recipe.insertMany(data)
+}).then(() => {
+  Recipe.updateOne({
+    duration: 100
+  })
+}).then(() => {
+  console.log('succesful updated recipe!');
+  //aqui haz lo siguiente del ejercicio
+})
+
+module.exports = Recipe;
